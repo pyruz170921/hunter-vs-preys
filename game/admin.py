97bba7101b55dcfa9f6a41e1639c3c7e93c8e5aa
Brayan Alexis Medina Lopez <pyruz17021@gmail.com>
2026-06-04 17:30:04 -0500

    Mejorar visualizacion de simulaciones en admin

--- game/admin.py
+++ game/admin.py
@@ -7,13 +7,74 @@ from .models import (
     Hunter,
     Prey,
     Movement,
-    GameEvent
+    GameEvent,
 )
 
-admin.site.register(Game)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "room",
+        "board_size",
+        "status",
+        "total_time",
+        "hunter_moves",
+        "start_time",
+        "end_time",
+    )
+
+    list_filter = (
+        "status",
+        "board_size",
+    )
+
+    search_fields = (
+        "room__name",
+        "room__code",
+    )
+
+
+@admin.register(Prey)
+class PreyAdmin(admin.ModelAdmin):
+    list_display = (
+        "game",
+        "number",
+        "alive",
+        "moves",
+        "survival_time",
+        "captured_at",
+        "ranking_position",
+        "algorithm",
+    )
+
+    list_filter = (
+        "alive",
+        "algorithm",
+        "ranking_position",
+    )
+
+
+@admin.register(Movement)
+class MovementAdmin(admin.ModelAdmin):
+    list_display = (
+        "game",
+        "turn",
+        "entity_type",
+        "entity_number",
+        "from_x",
+        "from_y",
+        "to_x",
+        "to_y",
+    )
+
+    list_filter = (
+        "entity_type",
+        "turn",
+    )
+
+
 admin.site.register(Board)
 admin.site.register(Obstacle)
 admin.site.register(Hunter)
-admin.site.register(Prey)
-admin.site.register(Movement)
 admin.site.register(GameEvent)
